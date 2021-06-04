b5b8fa3f4437b094fdf9c3e102c69fe7e8dfe440
GitHub Action <action@github.com>
2021-06-04 09:00:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/fdelporte/pi4jintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fdelporte/pi4jintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fdelporte.pi4jintellijplugin.listeners
 
+import com.github.fdelporte.pi4jintellijplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/fdelporte/pi4jintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fdelporte.pi4jintellijplugin.services
+
+import com.github.fdelporte.pi4jintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
